c258fafdf1e492fb66ea2ead8dd5b96c1cea7370
LuckyBastard15 <85241539+LuckyBastard15@users.noreply.github.com>
2021-11-18 11:08:16 -0600

    update

---
+++ New game/Assets/Scripts/Limites.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limites : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2f, 2f),Mathf.Clamp(transform.position.y, -4f, 4f), transform.position.z);
+    }
+
+}
